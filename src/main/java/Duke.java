d3c5c1cef5ab80df32fe163e339fa6e32e510c31
KaneQuah <kane_quah@u.nus.edu>
2019-09-09 16:57:31 +0800

    A-Classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -39,7 +39,7 @@ public class Duke {
         }
     }
     public static void main(String[] args) {
-        /* Writing Level-9.... */
+        /* Writing A-Classes.... */
         new Duke("list.txt").run();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
